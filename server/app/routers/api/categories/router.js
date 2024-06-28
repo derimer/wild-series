200300699425e1e61f1d63f614967de2e26634c9
derimer <jderimer@gmail.com>
2024-06-28 04:48:15 +0200

    "ok"

--- server/app/routers/api/categories/router.js
+++ server/app/routers/api/categories/router.js
@@ -5,7 +5,10 @@ const router = express.Router();
 /* ************************************************************************* */
 // Define Your API Routes Here
 /* ************************************************************************* */
-
+const { browse, read } = require("../../../controllers/categoryActions");
+// Route to get a list of items
+router.get("/", browse);
+router.get("/:id", read);
 /* Here you code */
 
 /* ************************************************************************* */
