56777d1a1a54802bf0b007401ba4e1fcacf93d17
derimer <jderimer@gmail.com>
2024-06-29 11:20:13 +0200

    ajout des tables dans schema.sql

--- server/database/schema.sql
+++ server/database/schema.sql
@@ -4,9 +4,26 @@ create table user (
   password varchar(255) not null
 );
 
-create table item (
-  id int unsigned primary key auto_increment not null,
-  title varchar(255) not null,
-  user_id int unsigned not null,
-  foreign key(user_id) references user(id)
+CREATE TABLE item (
+  id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT NOT NULL,
+  title VARCHAR(255) NOT NULL,
+  user_id INT UNSIGNED NOT NULL,
+  CONSTRAINT fk_user_id FOREIGN KEY (user_id) REFERENCES user(id)
 );
+
+CREATE TABLE category (
+  id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT NOT NULL,
+  name VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE program (
+  id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT NOT NULL,
+  title VARCHAR(255) NOT NULL,
+  synopsis TEXT NOT NULL,
+  poster VARCHAR(255) NOT NULL,
+  country VARCHAR(255) NOT NULL,
+  year INT,
+  category_id INT UNSIGNED,
+  CONSTRAINT fk_category_id FOREIGN KEY (category_id) REFERENCES category(id)
+);
+
